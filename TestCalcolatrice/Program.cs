1621107f37944410c042e09b3272895bbdd7292f
Antonino <antoninosegreto.93@gmail.com>
2017-10-01 21:37:05 +0200

    somma e differenza aggiunte

--- TestCalcolatrice/Program.cs
+++ TestCalcolatrice/Program.cs
@@ -4,28 +4,71 @@ namespace TestCalcolatrice
 {
     class Program
     {
+        public double InputDouble()
+        {
+            if (double.TryParse(Console.ReadLine(), out double inputD))
+            {
+                Console.WriteLine("Numero acquisito.\n");
+            }
+            else
+            {
+                Console.WriteLine("Errore nell'aquisizione. IL valore verrà settato a 0\n");
+                inputD = 0;
+            }
+            return inputD;
+        }
+
+
+
+
         static void Main(string[] args)
         {
-            int sel = 0;
+            Program prog = new Program();
+           
+
             double a = 0, b = 0;
             double ris = 0;
-
+            
             Console.WriteLine("Primo numero");
+            a = prog.InputDouble();
             Console.WriteLine("Secondo numero");
+            b = prog.InputDouble();
             Console.WriteLine("Scegli l'operazione da effettuare");
             Console.WriteLine("1)Somma");
             Console.WriteLine("2)Differenza");
-            switch (sel)
+            if (int.TryParse(Console.ReadLine(), out int sel))
             {
-                case 1:
-                    ris = a + b;
-                    break;
-                default:
-                    Console.WriteLine("Selezione non valida");
-                    break;
+                switch (sel)
+                {
+                    case 1:
+                        ris = a + b;
+                        Console.WriteLine("Risultato: " + ris);
+                        break;
+                    case 2:
+                        ris = a - b;
+                        Console.WriteLine("Risultato: " + ris);
+                        break;
+                    default:
+                        Console.WriteLine("Selezione non valida");
+                        break;
+                }
                 
-
+                Console.WriteLine("Premere un tasto qualsiasi per terminare");
+                Console.ReadLine();
             }
+            else
+            {
+                Console.WriteLine("Si è verificato un errore. L'applicazione è terminata.");
+                Console.WriteLine("Premere un tasto qualsiasi per terminare");
+                Console.ReadLine();
+            }
+            
+
+            
+           
         }
+
+        
+
     }
 }

1df1412620cda0372f24d5bb5c6e617643cd5591
Antonino <antoninosegreto.93@gmail.com>
2017-10-01 17:04:30 +0200

    buttata giù la struttura del programma

--- TestCalcolatrice/Program.cs
+++ TestCalcolatrice/Program.cs
@@ -6,7 +6,26 @@ namespace TestCalcolatrice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sel = 0;
+            double a = 0, b = 0;
+            double ris = 0;
+
+            Console.WriteLine("Primo numero");
+            Console.WriteLine("Secondo numero");
+            Console.WriteLine("Scegli l'operazione da effettuare");
+            Console.WriteLine("1)Somma");
+            Console.WriteLine("2)Differenza");
+            switch (sel)
+            {
+                case 1:
+                    ris = a + b;
+                    break;
+                default:
+                    Console.WriteLine("Selezione non valida");
+                    break;
+                
+
+            }
         }
     }
 }
